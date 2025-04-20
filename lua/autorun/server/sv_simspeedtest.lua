f8ca361b4d5e5bd6209b7fa5435ab422405c5d64
Mr Marty <57878680+MartyX5555@users.noreply.github.com>
2025-04-20 16:32:31 -0400

    new progress
    - re-organized convars
    - installing bases for the protection system
    - improved hook system. Not as messy as before

--- lua/autorun/server/sv_simspeedtest.lua
+++ lua/autorun/server/sv_simspeedtest.lua
@@ -25,17 +25,23 @@ hook.Add( "Initialize", "SimSpeed.Initialize.ConvarValueSync", function()
 	GSimSpeed.IsEnabled = tobool(value)
 end )
 
--- Values might vary between each computer's performance. Adjust it according to your tests and specs.
--- If used in a dedicated server
+-- SimSpeed Rules. For the best results, adjust it according to your current game performance.
+-- TODO: Provide a tool/helper to calibrate this properly.
+CreateConVar("gsimspeed_max_entities", 500, FCVAR_ARCHIVE, "Max number of entities to consider a good performance.")
+CreateConVar("gsimspeed_max_consscore", 6000, FCVAR_ARCHIVE, "Max Score obtained by props with constraints to consider a good performance.")
+CreateConVar("gsimspeed_max_coldelay", 7, FCVAR_ARCHIVE, "Max delay, between collisions to consider a good performance")
+CreateConVar("gsimspeed_max_luadelay", 30, FCVAR_ARCHIVE, "Max delay, for lua to consider a good performance") -- 15 can work, but for servers
 
-CreateConVar("gsimspeed_system_defaultsim", 1, FCVAR_ARCHIVE, "Sets the default sim speed.")
-CreateConVar("gsimspeed_system_max_entities", 500, FCVAR_ARCHIVE, "Max number of entities to consider a good performance.")
-CreateConVar("gsimspeed_system_max_consscore", 6000, FCVAR_ARCHIVE, "Max Score obtained by props with constraints to consider a good performance.")
-CreateConVar("gsimspeed_system_max_coldelay", 7, FCVAR_ARCHIVE, "Max delay, between collisions to consider a good performance")
-CreateConVar("gsimspeed_system_max_luadelay", 30, FCVAR_ARCHIVE, "Max delay, for lua to consider a good performance") -- 15 can work, but for servers
+-- Developer / Advanced settings. To enable/disable specific checks from the SimSpeed system.
+CreateConVar("gsimspeed_monitor_lua", 1, FCVAR_ARCHIVE, "enable/disable the lua/framerates check.")
+CreateConVar("gsimspeed_monitor_collisions", 1, FCVAR_ARCHIVE, "enable/disable the collisions check.")
+CreateConVar("gsimspeed_monitor_entities", 1, FCVAR_ARCHIVE, "enable/disable the entities/constraints check")
 
-CreateConVar("gsimspeed_props_cancreate_minsim", 0.05, FCVAR_ARCHIVE, "Sets the minimal sim speed prop spawning is allowed.")
-
+-- Protection and extras.
+CreateConVar("gsimspeed_enableprotection", 0, FCVAR_ARCHIVE, "enable/disable the default simspeed protection that consist on lockouts and freezes. Leave it disabled if you use a custom one.")
+CreateConVar("gsimspeed_defaultsim", 1, FCVAR_ARCHIVE, "Sets the default sim speed.")
+CreateConVar("gsimspeed_dangersim", 0.1, FCVAR_ARCHIVE, "Specifies the danger sim speed mark.")
+CreateConVar("gsimspeed_criticalsim", 0.02, FCVAR_ARCHIVE, "Specifies the critical sim speed mark.")
 
 GSimSpeed = GSimSpeed or {}
 GSimSpeed.Entities = GSimSpeed.Entities or {}
@@ -86,6 +92,10 @@ local function getConvarValue(command)
 	return GetConVar("gsimspeed_" .. command):GetFloat()
 end
 
+local function CanMonitor(sysname)
+	return getConvarValue("monitor_" .. (sysname or "")) >= 1
+end
+
 local function resetTimeScale()
 	if not GSimSpeed.IsSimSpeedActive then return end
 	GSimSpeed.IsSimSpeedActive = nil
@@ -93,13 +103,46 @@ local function resetTimeScale()
 	SetTimeScale( 1 )
 end
 
+-- EZ functions to use. If a server wants to implement this addon.
 -- I'm aware of physenv.SetPhysicsPaused() but that also pauses the GetLastSimulationTime too.
-local function FreezeProps(PhysObjs)
-	for object, _ in pairs(PhysObjs) do
+-- Freezes every prop on the map. Calls GSimSpeed.OnFreeze hook
+function GSimSpeed.FreezeAllProps()
+
+	local Entities = GSimSpeed.Entities
+	local Objects = {}
+	for ent, _ in pairs(Entities) do
+		if not IsValid(ent) then continue end
+
+		local object = ent:GetPhysicsObject()
 		if not IsValid(object) then continue end
+
 		object:EnableMotion(false)
+		Objects[object] = true
 	end
-	hook.Run( "SimSpeed_OnFreeze", PhysObjs )
+	hook.Run( "GSimSpeed.OnFreeze", Entities, Objects )
+end
+
+local TimerId = "GSimSpeed.LockoutTimer"
+
+local function TimerFunc()
+	if timer.RepsLeft( TimerId ) > 1 then return end
+	GSimSpeed.CanSpawn = true
+	hook.Run("SimSpeed.OnEndLockout")
+end
+
+-- Temporally prevents the creation of new props via spawnmenu during a specific period of time. 5 seconds is by default.
+function GSimSpeed.ApplyCreationLockout(CTime)
+
+	local Time = CTime or 5
+	if timer.Exists( TimerId ) then
+		timer.Adjust( TimerId, 1, Time, TimerFunc )
+		return
+	end
+	timer.Create( TimerId, 1, Time, TimerFunc )
+
+	GSimSpeed.CanSpawn = false
+
+	hook.Run("SimSpeed.OnStartLockout")
 end
 
 local previousratio = 1
@@ -112,66 +155,72 @@ local function SimSpeedTick()
 	end
 
 	-- Lag by lua. May also work with collisions at some point.
-	-- Note: this could not work in singleplayer in a reliable way. Use it at your own risk
-	local luafactor = engine.AbsoluteFrameTime() * 1000
-	local luaratio = math.pow(math.min( getConvarValue("system_max_luadelay") / luafactor, 2), 1)
+	-- Note: this could not work in singleplayer in a reliable way. Disable it by adding a big number.
+	local luaratio = 1
+	if CanMonitor("lua") then
+		local luafactor = engine.AbsoluteFrameTime() * 1000
+		luaratio = math.pow(math.min( getConvarValue("max_luadelay") / luafactor, 2), 1)
+	end
 
 	-- Lag by collisions.
-	local factor = physenv.GetLastSimulationTime() * 1000
-	local physratio = math.min(getConvarValue("system_max_coldelay") / factor, 1)
-
-	-- Lag by moving entities
-	local ActiveEnts = 0
-	local ActivePhysObjects = {}
-	local ExtraPoints = 0
-	for ent, _ in pairs(GSimSpeed.Entities) do
-		local physobj = ent:GetPhysicsObject()
-		if IsValid(physobj) and not physobj:IsAsleep() then
-			ActiveEnts = ActiveEnts + 1
-
-			-- The points are the constraints per entity.
-			if constraint.HasConstraints(ent) then
-				ExtraPoints = ExtraPoints + table.Count(ent.Constraints)
-			end
-
-			ActivePhysObjects[physobj] = true
-		end
+	local physratio = 1
+	if CanMonitor("collisions") then
+		local factor = physenv.GetLastSimulationTime() * 1000
+		physratio = math.min(getConvarValue("max_coldelay") / factor, 1)
 	end
-	local movratio = math.min(getConvarValue("system_max_entities") / ActiveEnts, 1)
 
-	-- Lag by constraints
-	local consratio = math.min(getConvarValue("system_max_consscore") / ExtraPoints, 1)
+	-- Lag by Entities & Constraints
+	local movratio = 1
+	local consratio = 1
+	if CanMonitor("entities") then
+
+		local ActiveEnts = 0
+		local ActivePhysObjects = {}
+		local ExtraPoints = 0
+		for ent, _ in pairs(GSimSpeed.Entities) do
+			local physobj = ent:GetPhysicsObject()
+			if IsValid(physobj) and not physobj:IsAsleep() then
+				ActiveEnts = ActiveEnts + 1
+
+				-- The points are the constraints per entity.
+				if constraint.HasConstraints(ent) then
+					ExtraPoints = ExtraPoints + table.Count(ent.Constraints)
+				end
+
+				ActivePhysObjects[physobj] = true
+			end
+		end
+		movratio = math.min(getConvarValue("max_entities") / ActiveEnts, 1)
+		consratio = math.min(getConvarValue("max_consscore") / ExtraPoints, 1)
+	end
 
 	-- Gets the worst of the 4. Apply an average based on the last ratio value.
 	local currentratio = math.min(physratio, movratio, consratio, luaratio)
 	local finalratio = (currentratio + previousratio + oldestratio) / 3
-	SetTimeScale( getConvarValue("system_defaultsim") *  finalratio )
-
-	-- Restricts the creation of new ents if the sim speed is below to the specified.
-	if GSimSpeed.CanSpawn and GetTimeScale() < getConvarValue("props_cancreate_minsim") then
-
-		local Override = hook.Run("SimSpeed.OnLockout", ActivePhysObjects)
-		if not Override then
-			FreezeProps(ActivePhysObjects)
-
-			GSimSpeed.CanSpawn = false
-			timer.Simple(5, function()
-				GSimSpeed.CanSpawn = true
-			end)
-		end
-	end
+	SetTimeScale( getConvarValue("defaultsim") *  finalratio )
 
 	oldestratio = previousratio
 	previousratio = currentratio
-end
 
+	local warninglevel = 0 -- normal. no warnings
+
+	if finalratio < getConvarValue("criticalsim") then -- Critical. Freeze props or another extreme measures. The part where the server is really dying.
+		warninglevel = 3
+	elseif finalratio < getConvarValue("dangersim") then -- Danger. Prevent more prop spawning or another restrictions.
+		warninglevel = 2
+	elseif finalratio < getConvarValue("defaultsim") then -- Warning. SimSpeed has droppen below default. No restrictions.
+		warninglevel = 1
+	end
+
+	hook.Run("SimSpeed.OnLowSim", GSimSpeed.Entities, finalratio, warninglevel)
+end
 hook.Remove("Tick", "SimSpeed.Tick")
 hook.Add("Tick", "SimSpeed.Tick", SimSpeedTick)
 
 local function CanCreateEntity(ply)
 
 	if not GSimSpeed.CanSpawn then
-		local Override = hook.Run( "SimSpeed.OnSpawnError", game.GetTimeScale() )
+		local Override = hook.Run( "SimSpeed.OnSpawnError", GetTimeScale() )
 		if not Override then
 			ply:ChatPrint("[SimSpeed Warning] - Too much lag to process this task!")
 		end
